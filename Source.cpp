8946da5acfbda717bd7d821e32553d23e3098f3e
BigK1337 <kyleahicks@yahoo.com>
2015-04-19 23:20:55 -0400

    Minor Changes
    Change I made to my section of coding.  Including including the include
    sstream to read my tokens.

--- Source.cpp
+++ Source.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <fstream>
 #include <Windows.h>
+#include <sstream>
 using namespace std;
 ifstream File, Vendor, Customer; //Three file initializations.
 string itemtotal; //Total number of items in Catalogue string form.
@@ -960,6 +961,7 @@ void saveProgress(warehouse num[3], int dayCount)
 
 		}
 	}
+}
 	void loadProgress(warehouse num[3], int dayCount)
 	{
 		string line;
@@ -1002,8 +1004,8 @@ void saveProgress(warehouse num[3], int dayCount)
 						{
 							if (token[3] != "Count:")
 							{
-								num[k].sloc[h].small[0] = token[3];
-								num[k].sloc[h].small[1] = token[5];
+								num[k].sloc[h].smalle[0] = token[3];
+								num[k].sloc[h].smalle[1] = token[5];
 							}
 							n++;
 							h++;
@@ -1012,8 +1014,8 @@ void saveProgress(warehouse num[3], int dayCount)
 						{
 							if (token[3] != "Count:")
 							{
-								num[k].sloc[h].small[0] = token[3];
-								num[k].sloc[h].small[1] = token[5];
+								num[k].sloc[h].smalle[0] = token[3];
+								num[k].sloc[h].smalle[1] = token[5];
 							}
 							n++;
 							h = 0;
@@ -1067,7 +1069,7 @@ void saveProgress(warehouse num[3], int dayCount)
 			{
 				cout << "Warehouse " << n + 1 << endl;
 				for (int i = 0; i < 20; i++){
-					cout << "Small " << i << " ID: " << num[n].sloc[i].small[0] << " Count: " << num[n].sloc[i].small[1] << endl;
+					cout << "Small " << i << " ID: " << num[n].sloc[i].smalle[0] << " Count: " << num[n].sloc[i].smalle[1] << endl;
 
 				}
 				system("pause");
@@ -1089,7 +1091,6 @@ void saveProgress(warehouse num[3], int dayCount)
 			cout << "Save file is not found." << endl;
 		}
 	}
-}
 int main()
 {
 	int holidayPick=0;
